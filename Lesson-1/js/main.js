7330cd596a949e1fb152941702f9ea93986e85f1
Egor Matiushkin <brodiaga89@gmail.com>
2021-02-26 13:23:30 +0300

    Coach Variant

--- Lesson-1/js/main.js
+++ Lesson-1/js/main.js
@@ -17,56 +17,68 @@ const products = [
     {id: 5}
 ];
 
-const renderProduct = (title = 'No title', price = startCalc('No ',' price')) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-}
-
-            //!!!Мы вставляли в разметку целый массив, и запятые-разделители элементов массива выводились 
-            // в нее.!!! 
-            // Так же, здесь  убираем лишние круглые и фигурные скобки, так как имеем лишь один аргумент массива
-            // и одну операцию в теле стрелочной функции.
+// const renderProduct = (title = 'No title', price = startCalc('No ',' price'), img = 'https://placehold.it/200x150') => {
+//     return `<div class="product-item">
+//                 <img src="${img}" alt="Some img">
+//                 <h3>${title}</h3>
+//                 <p>${price} \u20bd</p>
+//                 <button class="by-btn">Добавить в корзину</button>
+//               </div>`;
+// }
 
 
-            // Здесь мы методом перебора  сразу вставляем  каждый отдельный продукт в разметку.
+
+
+                // Здесь мы методом перебора  сразу вставляем  каждый отдельный продукт в разметку.#1
 // const renderProducts = list => 
     // list.forEach(item => 
     //     document.querySelector('.products').insertAdjacentHTML("beforeend",renderProduct(item.title, item.price))
     // );
 
 
+                // Думаю это тот самый топорный метод #2   
+// const renderProducts = list => {
+//     const productList = list.map((item) => {
+//         return renderProduct(item.title, item.price);
+//     });
     
-const renderProducts = list => {
-    const productList = list.map((item) => {
-        return renderProduct(item.title, item.price);
-    });
-    // Думаю это тот самый топорный метод
     // let [a,b,c,d,e] = productList;
     // document.querySelector('.products').insertAdjacentHTML("beforeend",a)
     // document.querySelector('.products').insertAdjacentHTML("beforeend",b)
     // document.querySelector('.products').insertAdjacentHTML("beforeend",c)
     // document.querySelector('.products').insertAdjacentHTML("beforeend",d)
 
-    // с помощью цикла
-    for(let i = 0; i < productList.length; i++){
-    document.querySelector('.products').insertAdjacentHTML("beforeend",productList[i]);
-    };
- 
-};
 
+                // с помощью цикла #3
+// const renderProducts = list => {
+//     const productList = list.map((item) => {
+//         return renderProduct(item.title, item.price);
+//     });    
+//     for(let i = 0; i < productList.length; i++){
+//     document.querySelector('.products').insertAdjacentHTML("beforeend",productList[i]);
+//     };
+ 
+// };
+
+
+// renderProducts(products);
+
+
+// TeacherVariant #4 Топорный метод .join() такое решение не актуально,чисто для демонстрации.
+
+const renderProduct = (item, img = 'https://placehold.it/200x150') => 
+        `<div class="product-item" data-id="${this.id}">
+            <img src="${img}" alt="Some img">
+            <div class="desc">
+                <h3>${item.title}</h3>
+                <p>${item.price} \u20bd</p>
+                <button class="by-btn">Buy</button>  
+            </div>
+        </div>`;
+
+const renderProducts = list => {
+document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(' '));
+};        
 
 renderProducts(products);
-
-// а вот метод "ревьюз" о котором говорилось в конце урока, я ничего не нашел, какие то есть умоминания
-// но в не понятном контексте. Скорее всего я его  на слух как то не так понял и неверно гуглю. 
-// Хотелось ыб про него почитать,тк вы нахваливали его))
-
-// Еще были мысли физически убрать запятую, сославшись на див после которого она стоит. Типа : div.remove('afterend')
-// Но никаких методов для подобного редактирования разметки не нашел, а к запятой напрямую не смог обратиться,
-// искал через отладчик как к ней зацепиться, но ни классов, ни тегов у нее нет. 
-
-// Если есть варианты решения, ссылки на статьи  и вас не затруднит, буду рад, я вам писал в телеге "Егор Матюшкин"
-
+     
