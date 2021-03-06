853c7f4c3dfc9380d9d53e2cd0cd1b82fea94c2b
Egor Matiushkin <brodiaga89@gmail.com>
2021-03-06 22:08:54 +0300

    Finish 3 lesson(my)

--- Lesson/js/main.js
+++ Lesson/js/main.js
@@ -14,11 +14,9 @@ const getProducts = (data) => {
       else reject('Error');
 });
 }
-
-
 class ProductList {
-    #goods;
-    #allProducts;
+    // #goods;
+    // #allProducts;
   
     constructor(container = '.products') {
       this.container = container;
@@ -27,8 +25,6 @@ class ProductList {
   
       //#Pr
       this.showProducts(`${API}/catalogData.json`);
-      
-
       // this.#fetchGoods()
       //         .then((data) => {
       //             this.#goods = [...data];
@@ -36,14 +32,10 @@ class ProductList {
       //          });;
   
     }
-
-
     showSum(){
       document.querySelector('.final_price')
       .insertAdjacentHTML('beforeend', `${this.allProducts.reduce((accum, item) => accum + item.price, 0)} \u20bd`);
     }
-
-
     //#Pr
     showProducts(url){
       let xhr = new XMLHttpRequest();
@@ -84,24 +76,28 @@ class ProductList {
         this.allProducts.push(productObject);
         block.insertAdjacentHTML('beforeend', productObject.render());
       });
+
     }
   }
   
   class ProductItem {
     constructor(product, img='https://placehold.it/200x150') {
-      this.title = product.product_name;
+      this.product_name = product.product_name;
       this.price = product.price;
-      this.id = product.id_product;
+      this.id_product = product.id_product;
       this.img = img;
     }
   
     render() {
-      return `<div class="product-item" data-id="${this.id}">
+      return `<div class="product-item" data-id="${this.id_product}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
-                    <h3>${this.title}</h3>
+                    <h3>${this.product_name}</h3>
                     <p>${this.price} \u20bd</p>
-                    <button class="buy-btn">Купить</button>
+                    <button class="buy-btn"
+                    data-id="${this.id_product}"
+                    data-name="${this.product_name}"
+                    data-price="${this.price}">Купить</button>
                 </div>
             </div>`;
     }
@@ -111,29 +107,109 @@ class ProductList {
   const productList = new ProductList();
   
   
-  //       //Корзина товаров
-  // class CartList {
-  //   constructor(){
-  //   }
   
-    //   getListArr() - Метод будет формировать массив из выбранных товаров. 
+        //Корзина товаров
+  class CartList {
+    constructor(container = '.cart_list'){
+      this.container = container;
+      this.goods = [];
+      this.allProducts = [];
+      this.finalPrice = 0;
+      this.getListArr()
+      this.cartArrDel()
+      this.showCart()
+      
+    }
+
+      showCart(){
+        let cartBtn = document.querySelector('.btn-cart');
+        let cartList = document.querySelector('.cart_list');
+        cartBtn.addEventListener('click', () => {
+          cartList.classList.toggle('cart_listHidden');
+        })
+      }
+
+
+      getListArr(){
+        document.querySelector('.products').addEventListener('click', elem => {
+          if(elem.target.classList.contains('buy-btn')){
+              let product = {
+                id_product : +elem.target.dataset['id'],
+                price: +elem.target.dataset['price'],
+                product_name: elem.target.dataset['name'],
+              }  
+             this.goods.push(product) 
+             this.getCart()
+             this.goods = []
+          }
+        });
+
+      }
+
+      getCart() {
+        
+        const cartBlock = document.querySelector(this.container);
+        this.goods.forEach(product => {
+          const cartObject = new Cart(product);
+          this.allProducts.push(product);
+          cartBlock.insertAdjacentHTML('afterbegin', cartObject.renderCart());
+          console.log(this.allProducts);
+          this.finalPrice += cartObject.price;
+          document.querySelector('.totalPrice').textContent = `${this.finalPrice} \u20bd`;
+
+        })
+
   
-    //   cartArrDel() - Будет убирать не нужный товар из массива.
+      }
   
-    //   cartArrRemove() -  Очищаем массив. 
-    // }
+      cartArrDel() {
+        document.querySelector(this.container).addEventListener('click', elem => {
+          if(elem.target.classList.contains('del-btn')){
+              let delProduct =  +elem.target.dataset['id']
+              let find = this.allProducts.find(product => product.id_product === delProduct);
+              this.allProducts.splice(this.allProducts.indexOf(find), 1);
+              document.querySelector(`.cart_item[data-id="${delProduct}"]`).remove();
+              this.finalPrice -= +find.price;
+
+          }
+          document.querySelector('.totalPrice').textContent = `${this.finalPrice} \u20bd`;
+
+        });
+      }
+
+    }
+
+    const cart = new CartList();
   
-    // class Cart() {
-    //   constructor()
-    // }
+
+
+    class Cart {
+      constructor(product, img='https://placehold.it/50x50', quantity= 1){
+        this.product_name = product.product_name;
+        this.price = product.price;
+        this.id_product = product.id_product;
+        this.img = img;
+        this.quantity = quantity;
+
+      }
+      renderCart(){
+        return `<div class="cart_item" data-id="${this.id_product}">
+                <div class="product-bio">
+                  <img src="${this.img}" alt="Some image">
+                <div class="product-desc">
+                  <p class="product-title">${this.product_name}</p>
+                  <p class="product-quantity">Количество: ${this.quantity}</p>
+                  <p class="product-single-price">${this.price} за ед.</p>
+                </div>
+              </div>
+              <div class="right-block">
+                <p class="product-price">${this.quantity*this.price} ₽</p>
+                <button class="del-btn" data-id="${this.id_product}">&times;</button>
+              </div>
+              </div>
+              `
+      }
   
-    //   showCart() - Метод будет выводить актуальный массив с товарами. 
-  
-    //   showFinalPrice() - будет выводить общую цену опираясь на актуальный масиив. 
-  
-    //   getDiscount() - Будет присваивать скидку опираясь на финальную цену.
-  
-  
-    
-  
-     
+
+    }
+
